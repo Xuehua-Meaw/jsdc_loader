ec27518130d0497012da8f96d79275b97a9dc5f7
Xuehua-Meaw <stanley09537000@gmail.com>
2025-07-04 18:55:23 +0800

    将版本号更新至0.2.2，确保与项目的最新状态一致喵～

--- src/jsdc_loader/__init__.py
+++ src/jsdc_loader/__init__.py
@@ -4,7 +4,7 @@ from .dumper import jsdc_dump, jsdc_dumps
 from .loader import jsdc_load, jsdc_loads
 
 __author__ = "Neko"
-__version__ = "0.2.1"  # 杂鱼♡～升级到2.1喵～
+__version__ = "0.2.2"  # 杂鱼♡～升级到2.1喵～
 __all__ = [
     # 杂鱼♡～传统函数，保持向后兼容喵～
     "jsdc_load", 
